d4fd9afb7ef053e711a232e0b83176728716ebbc
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2026-08-12 22:04:58 +0100

    Add nova APP arquiteturaspring

---
+++ arquiteturaspring/src/main/java/com/manuelluvuvamo/arquiteturaspring/ArquiteturaspringApplication.java
@@ -0,0 +1,13 @@
+package com.manuelluvuvamo.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaspringApplication.class, args);
+	}
+
+}
